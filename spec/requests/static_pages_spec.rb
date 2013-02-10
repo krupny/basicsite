c888c25f5a40322824c903f83bcb9ae34d39984e
KPNY <alpha.cyrus@gmail.com>
2013-02-10 06:32:15 -0500

    Unit tests added

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,11 +1,52 @@
 require 'spec_helper'
 
-describe "StaticPages" do
-  describe "GET /static_pages" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get static_pages_index_path
-      response.status.should be(200)
-    end
-  end
+describe "Static Page Tests" do
+	describe "Home Page" do
+		it "should have title 'Basic Site - Home'" do
+			visit '/static_pages/home'	
+			page.should have_selector('title', 
+				:text => 'Basic Site - Home')
+		end
+		it "should have h1 'Basic Site - Home'" do
+			visit '/static_pages/home'	
+			page.should have_selector('h1', 
+				:text => 'Basic Site - Home')
+		end
+	end
+	describe "Contact Page" do
+		it "should have title 'Basic Site - Contact'" do
+			visit '/static_pages/contact'	
+			page.should have_selector('title', 
+				:text => 'Basic Site - Contact')
+		end
+		it "should have h1 'Basic Site - Contact'" do
+			visit '/static_pages/contact'	
+			page.should have_selector('h1', 
+				:text => 'Basic Site - Contact')
+		end
+	end
+	describe "About Page" do
+		it "should have title 'Basic Site - About Us'" do
+			visit '/static_pages/about'	
+			page.should have_selector('title', 
+				:text => 'Basic Site - About Us')
+		end
+		it "should have h1 'Basic Site - About Us'" do
+			visit '/static_pages/about'	
+			page.should have_selector('h1', 
+				:text => 'Basic Site - About Us')
+		end
+	end
+	describe "Help Page" do
+		it "should have title 'Basic Site - Help'" do
+			visit '/static_pages/help'	
+			page.should have_selector('title', 
+				:text => 'Basic Site - Help')
+		end
+		it "should have h1 'Basic Site - Help'" do
+			visit '/static_pages/help'	
+			page.should have_selector('h1', 
+				:text => 'Basic Site - Help')
+		end
+	end
 end
